c554ba17bb8b8f13a593643595a64e83e035c9c2
Kylir <Kylir@users.noreply.github.com>
2017-08-22 18:41:37 +0100

    Change initialisation of the driver

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 let Oled = require('oled-i2c-bus')
 let font = require('oled-font-5x7')
+let i2c = require('i2c-bus'),
+    i2cBus = i2c.openSync(1)
 
-function display(driver, s) {
+var opts = {
+  width: 128,
+  height: 32,
+  address: 0x3C
+};
+
+var oled = new Oled(i2cBus, opts);
+
+function display(driver, font, s) {
     driver.setCursor(1, 1)
     driver.writeString(font, 1, s, 1, true)
 }
 
-let oled = new Oled()
 oled.clearDisplay()
 
 let counter = 0
-setInterval(display, 1000, oled, 'This is call number ' + ++counter)
-
+setInterval(display, 1000, oled, font, 'This is call number ' + ++counter)
