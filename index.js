fcb653da358539520e071679bea1342e58031ae3
Kylir <Kylir@users.noreply.github.com>
2017-08-22 17:30:17 +0100

    rename main function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let Oled = require('oled-ssd1306-i2c')
 let font = require('oled-font-5x7')
 
-function writeString(driver, s) {
+function display(driver, s) {
     driver.setCursor(1, 1)
     driver.writeString(font, 1, s, 1, true)
 }
@@ -9,5 +9,5 @@ function writeString(driver, s) {
 let oled = new Oled()
 let counter = 0
 
-setInterval(writeString, 1000, oled, 'This is call number ' + counter++)
+setInterval(display, 1000, oled, 'This is call number ' + counter++)
 
