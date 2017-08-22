8fe42a50a1fdd398f6f53c8c04bdfa3929ded4d7
Kylir <Kylir@users.noreply.github.com>
2017-08-22 17:22:44 +0100

    Fix call to Oled constructor.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let oled = require('oled-ssd1306-i2c')()
+let Oled = require('oled-ssd1306-i2c')
 let font = require('oled-font-5x7')
 
 function writeString(driver, s) {
@@ -6,6 +6,8 @@ function writeString(driver, s) {
     driver.writeString(font, 1, s, 1, true)
 }
 
+let oled = new Oled()
 let counter = 0
+
 setInterval(writeString, 1000, oled, 'This is call number ' + counter++)
 
